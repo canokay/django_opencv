f44bedca1f60bb783e63a7cb5eae5dec3ac870e0
Can Okay <canokay2014@gmail.com>
2020-03-12 23:31:02 +0300

    0.1.1 - django_opencv_app first migrate.

--- django_opencv_app/models.py
+++ django_opencv_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(null=False, max_length=40)
+    modified_image = models.ImageField(upload_to='modified_images/', null=True, blank=True)
+
+
+    def save(self):
+        self.modified_image.save(self.image.name, self.image, save=False)
+        super(Image, self).save()
+
